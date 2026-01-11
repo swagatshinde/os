389046ec673f1b964f42d3c3a5923ddb08d3f2a5
swagatshinde <htcswagatshinde@gmail.com>
2026-01-11 08:56:06 +0530

    Add files via upload

---
+++ s1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+void main()
+{
+    int n, m, i, j;
+    printf("Enter the number of resources (m): ");
+    scanf("%d", &m);
+    printf("Enter the number of processes (n): ");
+    scanf("%d", &n);
+    int available[m], work[m], allocation[10][10], max[10][10], need[10][10];
+    printf("\nEnter the available matrix (%d elements): ", m);
+    for (i = 0; i < m; i++)
+    {
+        scanf("%d", &available[i]);
+        work[i] = available[i];
+    }
+    printf("Enter the allocation matrix :\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &allocation[i][j]);
+    printf("The allocation matrix is :\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("\t%d", allocation[i][j]);
+        printf("\n");
+    }
+    printf("Enter the Max matrix :\n");
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            scanf("%d", &max[i][j]);
+    printf("The max matrix is :\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("\t%d", max[i][j]);
+        printf("\n");
+    }
+
+    for (i = 0; i < n; i++)
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - allocation[i][j];
+    printf("\n ** need matrix is ** \n ");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            printf("\t%d", need[i][j]);
+        printf("\n");
+    }
+
+    printf("\n ** Available Matrix ** \n");
+    for (i = 0; i < m; i++)
+    {
+        printf("\t%d", work[i]);
+    }
+}
